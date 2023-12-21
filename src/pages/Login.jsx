2ce1240ad1e778858619c9c5fdc502ef00491628
Razeroth <rizqi.ivah.21@student.if.ittelkom-sby.ac.id>
2023-12-22 02:30:07 +0700

    Add Hal Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
 
-export default Login
+  const navigate = useNavigate();
+
+  const { login } = useContext(AuthContext);
+
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login(inputs)
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+  return (
+    <div className="auth">
+      <h1>Login</h1>
+      <form>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Login</button>
+        {err && <p>{err}</p>}
+        <span>
+          Apakah kamu sudah punya akun? <Link to="/register">Register</Link>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
